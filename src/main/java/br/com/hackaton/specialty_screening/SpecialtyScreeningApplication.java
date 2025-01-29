14ac95805586f06b15eb017ab53edc24e395972f
Cleiton Melo Da Silva <cleiton.silva@gmmti.com.br>
2025-01-29 10:57:46 -0300

    Configuração do Projeto

---
+++ src/main/java/br/com/hackaton/specialty_screening/SpecialtyScreeningApplication.java
@@ -0,0 +1,13 @@
+package br.com.hackaton.specialty_screening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialtyScreeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialtyScreeningApplication.class, args);
+	}
+
+}
